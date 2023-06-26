0679f738a0f9c2aeb1a443c7e5909dcb72264bc6
HamsaOmar-dev <hamsaomar21@gmail.com>
2023-06-26 17:03:26 -0500

    more middleware logs

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ async function apiAccess(req: NextRequest, res: NextResponse) {
     "https://www.coursemind.co",
     "https://coursemind.co",
   ];
-  console.log(req.headers);
+  console.log(req.headers.get("referer"));
   if (
     req.nextUrl.pathname.startsWith("/api") &&
     !authorizedOrigins.includes(req.nextUrl.origin)
