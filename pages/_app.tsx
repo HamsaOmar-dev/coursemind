d5e62510829660490331b4e0574457bd1dfa994a
HamsaOmar-dev <hamsaomar21@gmail.com>
2023-06-29 13:02:02 -0500

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
+import type { AppProps } from "next/app";
+import Script from "next/script";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-MHYZ1HCMMT"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MHYZ1HCMMT');
+        `}
+    </Script>
+    return <Component {...pageProps} />;
+  </>;
 }
