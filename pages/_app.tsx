a93597a2e8648fd2ef5bb2e98e554f8f1bee0299
HamsaOmar-dev <hamsaomar21@gmail.com>
2023-06-29 17:49:59 -0500

    fixed empty page bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,22 @@ import Script from "next/script";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  <>
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-MHYZ1HCMMT"
-      strategy="afterInteractive"
-    />
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MHYZ1HCMMT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-MHYZ1HCMMT');
         `}
-    </Script>
-    return <Component {...pageProps} />;
-  </>;
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
